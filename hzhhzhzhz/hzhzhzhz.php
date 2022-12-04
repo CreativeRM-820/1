a1693722a387a2f07ed75409550653873a8efdc3
CreativeRM-820 <66487357+CreativeRM-820@users.noreply.github.com>
2022-12-04 20:42:49 +0300

    Add files via upload

---
+++ hzhhzhzhz/hzhzhzhz.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: hzhzhzhz
+Plugin URI: https://hzhzhzhz.com/
+Description: Used by millions, hzhzhzhz is quite possibly the best in the world
+Version: 0.1
+*/
+
+if(!defined('ABSPATH')){
+    exit;
+}
+
+class AleBooking
+{
+     function __construct(){
+        add_action('init',array($this,'custom_post_type'));
+     }
+     function custom_post_type(){
+        register_post_type('room',array(
+            'public' => true,
+            'label' => esc_html__( 'Новость', 'AleBooking'),
+            'supports'  => array('title','editor','author','thumbnail','comments')
+        ));
+     } 
+    
+
+}
+
+if(class_exists('AleBooking')){
+    new AleBooking('hzhzhz');
+}
+
+
+
+
+
+
+/*
+function say_salam(){
+    echo ('Salam');
+}
+add_action('admin_init','say_salam');
+*/
